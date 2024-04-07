601983666d02b01272354e9a7a6ac0166b8ec41b
swaran18999 <swaran18999@gmail.com>
2024-04-06 22:30:46 -0400

    dummy fetch test

--- roomio/app/routes/home.js
+++ roomio/app/routes/home.js
@@ -1,3 +1,21 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class HomeRoute extends Route {}
+export default Route.extend({
+  // ajax: service('ajax-fw'),
+  ajaxfw: service(),
+  setupController(controller) {
+    controller.set('currentRoute', this);
+  },
+  actions: {
+    abc() {
+      console.log('abc clicked');
+      this.ajaxfw.request('/trial').then(res=>{
+        console.log(res)
+      }, err=>{
+        console.log("err");
+        console.log(err)
+      });
+    },
+  },
+});
