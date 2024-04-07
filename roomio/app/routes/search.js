d4a47b9df671a48d17f8dacc148390e066e975f0
swaran18999 <swaran18999@gmail.com>
2024-04-07 14:18:35 -0400

    search functionality added

--- roomio/app/routes/search.js
+++ roomio/app/routes/search.js
@@ -1,3 +1,31 @@
 import Route from '@ember/routing/route';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default class SearchRoute extends Route {}
+export default class SearchRoute extends Route {
+	@service ajaxfw;
+	@tracked searchOutput = [];
+    setupController(controller) {
+        controller.set('hasData', false)
+        controller.set('currentRoute', this);
+    }
+    @action
+    handleSearchInput(event) {
+        this.controller.set("searchOutput", []);
+        let searchInput = event.target.value
+        // console.log('Search input:', searchInput);
+        let self = this;
+        this.ajaxfw.request("/search_unit/" + searchInput).then(
+            (res) => {
+                console.log(res);
+                self.controller.set("searchOutput", res.data)
+                self.controller.set("hasData", true)
+            },
+            (err) => {
+                console.log('err');
+                console.log(err);
+            }
+        );
+	}
+}
