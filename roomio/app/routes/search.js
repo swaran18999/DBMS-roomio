0c9cf4d5fd6091e3dbd8f0509fa69b3c2aec4f15
swaran18999 <swaran18999@gmail.com>
2024-04-07 14:25:04 -0400

    search functionality added

--- roomio/app/routes/search.js
+++ roomio/app/routes/search.js
@@ -4,28 +4,28 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class SearchRoute extends Route {
-	@service ajaxfw;
-	@tracked searchOutput = [];
-    setupController(controller) {
-        controller.set('hasData', false)
-        controller.set('currentRoute', this);
-    }
-    @action
-    handleSearchInput(event) {
-        this.controller.set("searchOutput", []);
-        let searchInput = event.target.value
-        // console.log('Search input:', searchInput);
-        let self = this;
-        this.ajaxfw.request("/search_unit/" + searchInput).then(
-            (res) => {
-                console.log(res);
-                self.controller.set("searchOutput", res.data)
-                self.controller.set("hasData", true)
-            },
-            (err) => {
-                console.log('err');
-                console.log(err);
-            }
-        );
-	}
+  @service ajaxfw;
+  @tracked searchOutput = [];
+  setupController(controller) {
+    controller.set('hasData', false);
+    controller.set('currentRoute', this);
+  }
+  @action
+  handleSearchInput(event) {
+    this.controller.set('searchOutput', []);
+    let searchInput = event.target.value;
+    // console.log('Search input:', searchInput);
+    let self = this;
+    this.ajaxfw.request('/search_unit/' + searchInput).then(
+      (res) => {
+        console.log(res);
+        self.controller.set('searchOutput', res.data);
+        self.controller.set('hasData', true);
+      },
+      (err) => {
+        console.log('err');
+        console.log(err);
+      }
+    );
+  }
 }
