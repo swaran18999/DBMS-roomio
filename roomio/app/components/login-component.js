4281e92e40c7d17ce43fe42db0ef9261258fa8d2
swaran18999 <swaran18999@gmail.com>
2024-04-24 16:53:49 -0400

    login component added

---
+++ roomio/app/components/login-component.js
@@ -0,0 +1,37 @@
+// app/components/pet-details.js
+import Component from '@ember/component';
+import { service } from '@ember/service';
+
+export default Component.extend({
+	router: service(),
+	ajaxfw: service(),
+	username: null,
+	password: null,
+	actions: {
+		createAccount() {
+			this.router.transitionTo('signup');
+		},
+		async login(event) {
+			let { username, password } = this.getProperties('username', 'password');
+
+			try {
+				this.ajaxfw.post('/login', { data: { username, password } }).then(
+					(res) => {
+						console.log(res);
+						if(res.flag == 1) {
+							this.router.transitionTo("home")
+						}
+						else {
+							this.set('errorMessage', "This username and password does not exist!");
+						}
+					},
+					(err) => {
+						console.log(err);
+					}
+				);
+			} catch (error) {
+				this.set('errorMessage', error.message);
+			}
+		},
+		},
+});
