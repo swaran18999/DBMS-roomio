c24925059ab21efeaedc9ac0a9eb5a861f4c8b6c
swaran18999 <swaran18999@gmail.com>
2024-04-24 17:08:49 -0400

    logout added

--- roomio/app/components/tab-components/nav-bar.js
+++ roomio/app/components/tab-components/nav-bar.js
@@ -1,3 +1,28 @@
-import Component from '@glimmer/component';
+import Component from '@ember/component';
+import { service } from '@ember/service';
 
-export default class TabComponentsNavBarComponent extends Component {}
+export default Component.extend({
+  router: service(),
+	ajaxfw: service(),
+	actions: {
+    logout() {
+      try {
+				this.ajaxfw.post('/logout').then(
+					(res) => {
+            if(res.flag == 1) {
+              this.router.transitionTo("login");
+            }
+            else {
+              console.error(res);
+            }
+					},
+					(err) => {
+            console.error(err);
+					}
+				);
+			} catch (error) {
+        console.error(error);
+			}
+    }
+	},
+});
