df71de2e7c8e3e7b863ed40250ea78afabe74134
swaran18999 <swaran18999@gmail.com>
2024-04-25 12:36:49 -0400

    lint fix

--- roomio/app/components/apitest-component.js
+++ roomio/app/components/apitest-component.js
@@ -3,29 +3,29 @@ import Component from '@ember/component';
 import { service } from '@ember/service';
 
 export default Component.extend({
-	ajaxfw: service(),
-	router: service(),
-	apiName: '',
-	method: 'POST',
-	data: '',
-	actions: {
-		methodChanged(event) {
-			this.set('method', event.target.value);
-		},
-		sendAPI() {
-			this.ajaxfw
-				.request(this.apiName, {
-					data: this.data.trim() == "" ? "" : JSON.parse(this.data),
-					method: this.method
-				})
-				.then(
-					(res) => {
-						console.log(res);
-					},
-					(err) => {
-						console.error(err);
-					}
-				);
-		},
-	},
+  ajaxfw: service(),
+  router: service(),
+  apiName: '',
+  method: 'POST',
+  data: '',
+  actions: {
+    methodChanged(event) {
+      this.set('method', event.target.value);
+    },
+    sendAPI() {
+      this.ajaxfw
+        .request(this.apiName, {
+          data: this.data.trim() == '' ? '' : JSON.parse(this.data),
+          method: this.method,
+        })
+        .then(
+          (res) => {
+            console.log(res);
+          },
+          (err) => {
+            console.error(err);
+          }
+        );
+    },
+  },
 });
