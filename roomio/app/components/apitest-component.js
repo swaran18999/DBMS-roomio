29d41d1ccf87baa6b25b28d7d53548ed909ff331
swaran18999 <swaran18999@gmail.com>
2024-04-24 22:21:37 -0400

    api test added

---
+++ roomio/app/components/apitest-component.js
@@ -0,0 +1,31 @@
+// app/components/pet-details.js
+import Component from '@ember/component';
+import { service } from '@ember/service';
+
+export default Component.extend({
+	ajaxfw: service(),
+	router: service(),
+	apiName: '',
+	method: 'POST',
+	data: '',
+	actions: {
+		methodChanged(event) {
+			this.set('method', event.target.value);
+		},
+		sendAPI() {
+			this.ajaxfw
+				.request(this.apiName, {
+					data: JSON.parse(this.data),
+					method: this.method
+				})
+				.then(
+					(res) => {
+						console.log(res);
+					},
+					(err) => {
+						console.error(err);
+					}
+				);
+		},
+	},
+});
