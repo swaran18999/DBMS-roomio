4088aa5eac81de3a4a3d3c732e9c32060a082be4
swaran18999 <swaran18999@gmail.com>
2024-04-25 12:59:19 -0400

    icon build changes

--- roomio/ember-cli-build.js
+++ roomio/ember-cli-build.js
@@ -4,6 +4,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
+    svgJar: {
+      sourceDirs: [
+        'node_modules/material-design-icons/file/svg/design',
+        'node_modules/material-design-icons/action/svg/design',
+        'public/icons',
+      ],
+    },
     // Add options here
   });
 
