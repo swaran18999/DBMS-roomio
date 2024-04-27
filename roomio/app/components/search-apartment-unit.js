e9401591dbb89ec6378e2ed77e986cd24a5a2ed1
swaran18999 <swaran18999@gmail.com>
2024-04-26 20:28:11 -0400

    linting changes

--- roomio/app/components/search-apartment-unit.js
+++ roomio/app/components/search-apartment-unit.js
@@ -3,34 +3,34 @@ import Component from '@ember/component';
 import { service } from '@ember/service';
 
 export default Component.extend({
-	ajaxfw: service(),
-	router: service(),
-	buildingName: 'Mary Island',
-	companyName: 'Ramos Inc',
+  ajaxfw: service(),
+  router: service(),
+  buildingName: 'Mary Island',
+  companyName: 'Ramos Inc',
 
-	actions: {
-		goToUnit(UnitRentID) {
-			this.router.transitionTo("unit", UnitRentID)
-		},
-		searchApartments() {
-			this.set("searchOutputUnits", null);
-			let data = {
-				companyName: this.companyName,
-				buildingName: this.buildingName
-			};
-			this.ajaxfw
-				.post('/search_apartment', {
-					data: data,
-				})
-				.then(
-					(res) => {
-						this.set("searchOutputUnits", res.data);
-						console.log(res);
-					},
-					(err) => {
-						console.error(err);
-					}
-				);
-		},
-	},
+  actions: {
+    goToUnit(UnitRentID) {
+      this.router.transitionTo('unit', UnitRentID);
+    },
+    searchApartments() {
+      this.set('searchOutputUnits', null);
+      let data = {
+        companyName: this.companyName,
+        buildingName: this.buildingName,
+      };
+      this.ajaxfw
+        .post('/search_apartment', {
+          data: data,
+        })
+        .then(
+          (res) => {
+            this.set('searchOutputUnits', res.data);
+            console.log(res);
+          },
+          (err) => {
+            console.error(err);
+          }
+        );
+    },
+  },
 });
--- roomio/app/routes/unit.js
+++ roomio/app/routes/unit.js
@@ -2,25 +2,28 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
-	ajaxfw: service(),
-	router: service(),
-		beforeModel(params) {
-			console.log(params.to.params['unitID'])
-			let unitID = params.to.params['unitID'];
-			this.ajaxfw.request('/search_unit/' + unitID).then(
-				(res) => {
-					let data = res.data;
-					this.controller.set("AvailableDateForMoveIn", data.AvailableDateForMoveIn)
-					this.controller.set("MonthlyRent", data.MonthlyRent)
-					this.controller.set("UnitNumber", data.UnitNumber)
-					this.controller.set("squareFootage", data.squareFootage)
-				},
-				(err) => {
-					if (err.status == 401) {
-						this.router.transitionTo('login');
-					}
-					console.error(err);
-				}
-				);
-		}
+  ajaxfw: service(),
+  router: service(),
+  beforeModel(params) {
+    console.log(params.to.params['unitID']);
+    let unitID = params.to.params['unitID'];
+    this.ajaxfw.request('/search_unit/' + unitID).then(
+      (res) => {
+        let data = res.data;
+        this.controller.set(
+          'AvailableDateForMoveIn',
+          data.AvailableDateForMoveIn
+        );
+        this.controller.set('MonthlyRent', data.MonthlyRent);
+        this.controller.set('UnitNumber', data.UnitNumber);
+        this.controller.set('squareFootage', data.squareFootage);
+      },
+      (err) => {
+        if (err.status == 401) {
+          this.router.transitionTo('login');
+        }
+        console.error(err);
+      }
+    );
+  },
 });
