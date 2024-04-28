3cc915f5765211fbf5768cb29e15ea908b6a78d2
ajay <krishajay.g@gmail.com>
2024-04-27 23:13:49 -0400

    added search by zipcode

--- roomio/app/routes/search-by-zipcode.js
+++ roomio/app/routes/search-by-zipcode.js
@@ -1,42 +1,16 @@
-// import Component from '@ember/component';
-// import { service } from '@ember/service';
-
-// export default Component.extend({
-//     ajaxfw: service(),
-//     router: service(),
-//     zipcode: 30113,
-  
-//     actions: {
-//       search_by_zipcode() {
-//         this.set('searchOutputUnits', null);
-//         this.ajaxfw
-//           .request('/search_by_zipcode/'+this.zipcode).then(
-//             (res) => {
-//               this.set('searchOutputUnits', res.data);
-//               console.log(res);
-//             },
-//             (err) => {
-//               console.error(err);
-//             }
-//           );
-//       },
-//     },
-//   });
-
-
 import Route from '@ember/routing/route';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 export default class SearchByZipcodeRoute extends Route {
     @service ajaxfw;
     setupController(controller) {
-        controller.set('hasData', false);
+        controller.set('hasData', "");
         controller.set('currentRoute', this);
     }
     @action
     search_by_zipcode() {
         this.controller.set('searchOutput', []);
-        this.controller.set('hasZipcodeData',false);
+        // this.controller.set('hasZipcodeData',false);
         // let zipcode = this.controller.target.zipcode;
         console.log(this.controller.zipcode);
         let self = this;
