24c703fc0ba04464651272818c049ec16f707e4a
ajay <krishajay.g@gmail.com>
2024-04-27 22:34:52 -0400

    pushed building.js from main

--- roomio/app/routes/building.js
+++ roomio/app/routes/building.js
@@ -8,33 +8,45 @@ export default Route.extend({
     console.log(params.to.params['company']);
     let company = params.to.params['company'];
     let building = params.to.params['building'];
-    console.log(company, building);
-    this.ajaxfw
-      .post('/search_building', {
-        data: {
-          companyName: company,
-          buildingName: building,
-        },
-      })
-      .then(
-        (res) => {
-          let data = res.data;
-          this.controller.set('CompanyName', data.CompanyName);
-          this.controller.set('BuildingName', data.BuildingName);
-          this.controller.set('Address', data.Address);
-          this.controller.set('amenitieslist', data.amenitieslist);
-          this.controller.set('YearBuilt', data.YearBuilt);
-          this.controller.set(
-            'NumUnitsAvailableForRent',
-            data.NumUnitsAvailableForRent
-          );
-        },
-        (err) => {
-          if (err.status == 401) {
-            this.router.transitionTo('login');
-          }
-          console.error(err);
+    console.log(company, building)
+    this.ajaxfw.post('/search_building', {
+      data: {
+        "companyName": company,
+        "buildingName": building
+      }
+    }).then(
+      (res) => {
+        let data = res.data;
+        this.controller.set('CompanyName', data.CompanyName);
+        this.controller.set('BuildingName', data.BuildingName);
+        this.controller.set('Address', data.Address);
+        this.controller.set('amenitieslist',data.amenitieslist);
+        this.controller.set('YearBuilt',data.YearBuilt);
+        this.controller.set('NumUnitsAvailableForRent',data.NumUnitsAvailableForRent);
+      },
+      (err) => {
+        if (err.status == 401) {
+          this.router.transitionTo('login');
         }
-      );
+        console.error(err);
+      }
+    );
+    this.ajaxfw.post('/check_pet_policy_compatibility', {
+      data: {
+        "companyName": company,
+        "buildingName": building
+      }
+    }).then(
+      (res) => {
+        this.controller.set('compatible_pets', res.data);
+      },
+      (err) => {
+        this.controller.set('compatible_pets', []);
+        if (err.status == 401) {
+          this.router.transitionTo('login');
+        }
+        console.error(err);
+      }
+    );
   },
 });
